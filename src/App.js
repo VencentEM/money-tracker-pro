ddc6d94387dc64bcdb9ee2db563a8ebd52dbb31d
VENCENT <luxestore1997@gmail.com>
2024-12-22 23:02:46 +0100

    stop in the minute 11:50

--- src/App.js
+++ src/App.js
@@ -14,8 +14,40 @@ function App() {
           <input type="text" placeholder={'descreption'}></input>
         </div>
         <button type="submit">Add new transaction</button>
-
       </form>
+      <div className='transactions'>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>New Samsung TV</div>
+            <div className='description'>It was time for new TV</div>
+          </div>
+          <div className='right'>
+            <div className='price'>$500</div>
+            <div className='datetime'>22-12-18 15:45</div>
+          </div>
+        </div>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>New Samsung TV</div>
+            <div className='description'>It was time for new TV</div>
+          </div>
+          <div className='right'>
+            <div className='price'>$500</div>
+            <div className='datetime'>22-12-18 15:45</div>
+          </div>
+        </div>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>New Samsung TV</div>
+            <div className='description'>It was time for new TV</div>
+          </div>
+          <div className='right'>
+            <div className='price'>$500</div>
+            <div className='datetime'>22-12-18 15:45</div>
+          </div>
+        </div>
+      </div>
+
     </main>
   );
 }
